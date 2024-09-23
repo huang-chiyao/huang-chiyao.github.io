cb7237eca809b8cae90c3b6b2f042bf3a34d2b3b
Chi-Yao Huang <chuan166@asu.edu>
2024-09-23 11:21:45 -0700

    [IO] Update bio, links, products

--- build.py
+++ build.py
@@ -7,23 +7,23 @@ def get_personal_data():
     github = "huang-chiyao"
     linkedin = "chi-yao-huang"
     bio_text = f"""
-                <p>Hi there, welcome to my place. This is Chi-Yao Huang.</p>
+                <p>Hi there! Welcome to my personal space. I'm Chi-Yao Huang.</p>
 
                 <p>
-                    I am currently a PhD student working with <a href="https://yezhouyang.engineering.asu.edu/">Yezhou Yang</a>
-                    at at School of Computing and Augmented Intelligence (SCAI), Arizona State University. 
-                    Before coming to ASU, I was a foundational VR/AR engineer of <a href="https://www.vive.com/us/">HTC VIVE</a>. 
-                    I developed a VR/AR tracking technique, which is used in almost all VR/AR products in HTC VIVE. 
-                    Previously, I received my master's degree in Mechanical Engineering at the National Taiwan University.  
+                    I'm currently pursuing my PhD under the guidance of <a href="https://yezhouyang.engineering.asu.edu/">Yezhou Yang</a> 
+                    at the School of Computing and Augmented Intelligence (SCAI), Arizona State University. 
+                    Prior to joining ASU, I worked as a foundational VR/AR engineer at <a href="https://www.vive.com/us/">HTC VIVE</a>, 
+                    where I developed VR/AR tracking technologies that are now integrated into almost all HTC VIVE products. 
+                    I hold a master's degree in Mechanical Engineering from National Taiwan University.  
                 </p>
                 <p>
-                    My research interests lie on 3D reconstruction, learning-based large-scale visual odometry, 
-                    and 3D semantic understanding in complex and dynamic environments.
+                    My research focuses on learning-based large-scale visual odometry, SLAM, spatial understanding, and spatial intelligence.
+                </p>
                 </p>
                 <p>
                     <a href="https://huang-chiyao.github.io/assets/pdf/CV.pdf" target="_blank" style="margin-right: 15px"><i class="fa fa-address-card fa-lg"></i> CV</a>
                     <a href="cy.huang@asu.edu" style="margin-right: 15px"><i class="far fa-envelope-open fa-lg"></i> Mail</a>
-                    <a href="" target="_blank" style="margin-right: 15px"><i class="fab fa-twitter fa-lg"></i> Twitter</a>
+                    <a href="https://x.com/chiyao_huang" target="_blank" style="margin-right: 15px"><i class="fab fa-twitter fa-lg"></i> Twitter</a>
                     <a href="" target="_blank" style="margin-right: 15px"><i class="fa-solid fa-book"></i> Scholar</a>
                     <a href="https://github.com/huang-chiyao" target="_blank" style="margin-right: 15px"><i class="fab fa-github fa-lg"></i> Github</a>
                     <a href="https://www.linkedin.com/in/chi-yao-huang" target="_blank" style="margin-right: 15px"><i class="fab fa-linkedin fa-lg"></i> LinkedIn</a>
@@ -42,45 +42,6 @@ def get_personal_data():
 def get_author_dict():
     return {
         # 'Andreas Geiger': 'https://www.cvlibs.net/',
-        # 'Songyou Peng': 'https://pengsongyou.github.io/',
-        # 'Zehao Yu': 'https://niujinshuchong.github.io/',
-        # 'Torsten Sattler': 'https://tsattler.github.io/',
-        # 'Katja Schwarz': 'https://katjaschwarz.github.io/',
-        # 'Axel Sauer': 'https://axelsauer.com/',
-        # 'Jonathan Barron': 'https://jonbarron.info/',
-        # 'Ben Mildenhall': 'https://bmild.github.io/',
-        # 'Mehdi Sajjadi': 'https://msajjadi.com/',
-        # 'Noha Radwan': 'http://www2.informatik.uni-freiburg.de/~radwann/',
-        # 'Chiyu Jiang': 'https://www.maxjiang.ml/',
-        # 'Yiyi Liao': 'https://yiyiliao.github.io/',
-        # 'Marc Pollefeys': 'https://people.inf.ethz.ch/pomarc/',
-        # 'Michael Oechsle': 'https://moechsle.github.io/',
-        # 'Christian Reiser': 'https://creiser.github.io/',
-        # 'Lars Mescheder': 'https://scholar.google.de/citations?user=h2k1gL4AAAAJ&hl=de',
-        # 'Thilo Strauss': 'https://scholar.google.com/citations?user=VlymtLQAAAAJ&hl=en',
-        # 'Sebastian Nowozin': 'http://www.nowozin.net/sebastian/',
-        # 'Marie-Julie Rakotosaona': 'http://www.lix.polytechnique.fr/Labo/Marie-Julie.RAKOTOSAONA/',
-        # 'Fabian Manhardt': 'https://campar.in.tum.de/Main/FabianManhardt',
-        # 'Diego Martin Arroyo': 'https://martinarroyo.net/',
-        # 'Abhijit Kundu': 'https://abhijitkundu.info/',
-        # 'Federico Tombari': 'https://www.cs.cit.tum.de/camp/members/senior-research-scientists/federico-tombari/',
-        # 'Anpei Chen': 'https://apchenstu.github.io/',
-        # 'Bozidar Antic': 'https://bozidarantic.com/',
-        # 'Apratim Bhattacharyya': 'https://apratimbhattacharyya18.github.io/',
-        # 'Siyu Tang': 'https://inf.ethz.ch/people/person-detail.MjYyNzgw.TGlzdC8zMDQsLTg3NDc3NjI0MQ==.html',
-        # 'Hidenobu Matsuki': 'https://dblp.org/pid/225/4487.html',
-        # 'Keisuke Tateno': 'https://campar.in.tum.de/Main/KeisukeTateno',
-        # 'Alessio Tonioni': 'https://alessiotonioni.github.io/',
-        # 'Christina Tsalicoglou': 'https://scholar.google.ch/citations?user=7D10QQkAAAAJ&hl=en', 
-        # 'Amit Raj': 'https://amitraj93.github.io/',
-        # 'Srinivas Kaza': 'https://www.linkedin.com/in/srinivas-kaza-64223b74',
-        # 'Ben Poole': 'https://poolio.github.io/',
-        # 'Nataniel Ruiz': 'https://natanielruiz.github.io/',
-        # 'Shiran Zada': 'https://scholar.google.com/citations?user=I2qheksAAAAJ',
-        # 'Kfir Aberman': 'https://kfiraberman.github.io/',
-        # 'Michael Rubinstein': 'http://people.csail.mit.edu/mrub/',
-        # 'Yuanzhen Li': 'http://people.csail.mit.edu/yzli/',
-        # 'Varun Jampani': 'https://varunjampani.github.io/'
         }
 
 def generate_person_html(persons, connection=", ", make_bold=True, make_bold_name='Chi-Yao Huang', add_links=True):
@@ -163,6 +124,19 @@ def get_publications_html():
         s+= get_paper_entry(k, bib_data.entries[k])
     return s
 
+def get_products_html():
+    products = [
+        {"name": "VIVE Focus", "link": "https://www.vive.com/us/product/vive-focus3/overview/"},
+        {"name": "VIVE XR", "link": "https://www.vive.com/us/product/vive-xr-elite/overview/"},
+        {"name": "VIVE Flow", "link": "https://www.vive.com/us/product/vive-flow/overview/"},
+    ]
+    
+    s = "<ul>"
+    for product in products:
+        s += f'<li><a href="{product["link"]}" target="_blank">{product["name"]}</a></li>'
+    s += "</ul>"
+    return s
+
 def get_talks_html():
     parser = bibtex.Parser()
     bib_data = parser.parse_file('talk_list.bib')
@@ -175,7 +149,9 @@ def get_talks_html():
 def get_index_html():
     pub = get_publications_html()
     talks = get_talks_html()
+    products = get_products_html()
     name, bio_text, footer = get_personal_data()
+    
     s = f"""
     <!doctype html>
 <html lang="en">
@@ -208,6 +184,14 @@ def get_index_html():
                 <img src="assets/img/profile.jpg" class="img-thumbnail" width="280px" alt="Profile picture">
             </div>
         </div>
+        
+        <div class="row" style="margin-top: 1em;">
+            <div class="col-sm-12" style="">
+                <h4>Products</h4>
+                {products}
+            </div>
+        </div>
+
         <div class="row" style="margin-top: 1em;">
             <div class="col-sm-12" style="">
                 <h4>Publications</h4>
@@ -226,12 +210,11 @@ def get_index_html():
     </div>
 
     <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
       integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
       crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
-      integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
+      integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7HUiibX39j7fakFPskvXusvfa0b4Q"
       crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
       integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
@@ -241,6 +224,7 @@ def get_index_html():
 </html>
     """
     return s
+
 
 
 def write_index_html(filename='index.html'):
